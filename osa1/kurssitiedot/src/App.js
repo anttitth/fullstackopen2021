f4b3d6c40ce36ffe097c13c765028d7d9dfc872d
anttitth <an_haka@hotmail.com>
2021-04-10 12:38:15 +0300

    viimeinen lisäys

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -2,19 +2,28 @@ import React from 'react'
 
 const Header = (props) => {
   return (
-    <di>
+    <div>
       <h1>{props.course}</h1>
-    </di>
+    </div>
   )
 }
 
 const Content = (props) => {
   return (
     <div>
-      <p>
-        {props.part} {props.exercises}
-      </p>
+      <Part part={props.part} exercises={props.exercises} />    
     </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>
+      {props.part} {props.exercises}
+      </p>     
+    </div>
+
   )
 }
 
@@ -45,6 +54,7 @@ const App = () => {
       <Content part={part1} exercises={exercises1} />
       <Content part={part2} exercises={exercises2} />
       <Content part={part3} exercises={exercises3} />
+
       <Total exercises={exercises1 + exercises2 + exercises3} />
 
     </div>
